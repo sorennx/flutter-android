7290277cbb1d77d165cbb163e1959fd407d5e159
sorennx <sorennx@gmail.com>
2023-03-28 21:19:05 +0200

    Async file loading

--- app_1/lib/main.dart
+++ app_1/lib/main.dart
@@ -29,7 +29,7 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: const MyHomePage(title: 'Flutter Android Lab'),
     );
   }
 }
@@ -53,6 +53,15 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  @override
+  void initState() {
+    super.initState();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      getTranslations().then((value) {
+      });
+    });
+  }
+
   final _formKey = GlobalKey<FormState>();
   final _nameController = TextEditingController();
   final _lastNameController = TextEditingController();
@@ -68,13 +77,12 @@ class _MyHomePageState extends State<MyHomePage> {
       print(isFormValid);
     });
   }
+
   XmlDocument translations = XmlDocument();
-  Future getTranslations() async {
-    // File file = File('assets/en_en.xml');
+  Future<XmlDocument> getTranslations() async {
     String xmlString = await loadAsset();
     translations = XmlDocument.parse(xmlString);
-    // print(translations);
-    // return await XmlDocument.parse(xmlString);
+    return translations;
   }
 
   Future<String> loadAsset() async {
@@ -83,111 +91,143 @@ class _MyHomePageState extends State<MyHomePage> {
 
   final user_lang = "en-en";
 
-  
-  // 
-  // XmlDocument a = XmlDocument();
-
-  // String to = document.findAllElements('to').first.text;
-  // String from = document.findAllElements('from').first.text;
-  // String heading = document.findAllElements('heading').first.text;
-  // String body = document.findAllElements('body').first.text;
-  // XmlDocument translations = XmlDocument();
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    // translations = getTranslations();
-    // Future<XmlDocument> translationsFuture = getTranslations();
-    // XmlDocument translations = await translationsFuture; // Allowed
-    // translations = getTranslations() as XmlDocument;
-    // print(translations);
-    getTranslations();
-    // print(translations.findAllElements("button_enter_grades_amount_label").first.text);
-    return Scaffold(
-        appBar: AppBar(
-          // Here we take the value from the MyHomePage object that was created by
-          // the App.build method, and use it to set our appbar title.
-          title: Text(widget.title),
-        ),
-        body: Form(
-            key: _formKey,
-            onChanged: () => setState(
-                () => _showSubmitButton = _formKey.currentState!.validate()),
-            child: Padding(
-                padding: const EdgeInsets.all(16.0),
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: TextFormField(
-                          controller: _nameController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return translations
-                                  .findAllElements('button_enter_name_text').first.text;
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            labelText: 'Name',
-                            hintText: 'Please enter your name',
-                            border: OutlineInputBorder(),
-                          ),
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: TextFormField(
-                          controller: _lastNameController,
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter your lastname';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            labelText: 'Lastname',
-                            hintText: 'Please enter your lastname',
-                            border: OutlineInputBorder(),
-                          ),
-                        )),
-                    Padding(
-                        padding: const EdgeInsets.all(5.0),
-                        child: TextFormField(
-                          controller: _gradesCountController,
-                          keyboardType: TextInputType.number,
-                          inputFormatters: [
-                            FilteringTextInputFormatter.digitsOnly
-                          ],
-                          validator: (value) {
-                            if (value == null || value.isEmpty) {
-                              return 'Please enter amount of your grades';
-                            }
-                            if (int.parse(value) < 5 || int.parse(value) > 15) {
-                              return 'The amount of grades needs to be in range [5,15]';
-                            }
-                            return null;
-                          },
-                          decoration: const InputDecoration(
-                            labelText: 'Grades count',
-                            hintText: 'Please enter amount of your grades',
-                            border: OutlineInputBorder(),
-                          ),
-                        )),
-                    Visibility(
-                        visible: _showSubmitButton,
-                        child: ElevatedButton(
-                          child: const Text('OCENY'),
-                          onPressed: () {
-                            if (_formKey.currentState!.validate()) {
-                              print(_nameController.text);
-                            }
-                          },
-                        ))
-                  ],
-                ))));
+    return FutureBuilder<XmlDocument>(
+      future: getTranslations(),
+      // The builder function will be called when the Future completes
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          // The Future has completed, so build the widget
+          // print(snapshot.data!.findAllElements("button_enter_grades_amount_label").first.text);
+          return Scaffold(
+              appBar: AppBar(
+                // Here we take the value from the MyHomePage object that was created by
+                // the App.build method, and use it to set our appbar title.
+                title: Text(widget.title),
+              ),
+              body: Form(
+                  key: _formKey,
+                  onChanged: () => setState(() =>
+                      _showSubmitButton = _formKey.currentState!.validate()),
+                  child: Padding(
+                      padding: const EdgeInsets.all(16.0),
+                      child: Column(
+                        children: <Widget>[
+                          Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: TextFormField(
+                                controller: _nameController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return translations
+                                        .findAllElements(
+                                            'button_enter_name_text')
+                                        .first
+                                        .text;
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  labelText: snapshot.data!
+                                      .findAllElements(
+                                          "button_enter_name_label")
+                                      .first
+                                      .text,
+                                  hintText: snapshot.data!
+                                      .findAllElements("button_enter_name_hint")
+                                      .first
+                                      .text,
+                                  border: OutlineInputBorder(),
+                                ),
+                              )),
+                          Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: TextFormField(
+                                controller: _lastNameController,
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return snapshot.data!
+                                        .findAllElements(
+                                            "button_enter_lastname_text")
+                                        .first
+                                        .text;
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  labelText: snapshot.data!
+                                      .findAllElements(
+                                          "button_enter_lastname_label")
+                                      .first
+                                      .text,
+                                  hintText: snapshot.data!
+                                      .findAllElements(
+                                          "button_enter_lastname_hint")
+                                      .first
+                                      .text,
+                                  border: OutlineInputBorder(),
+                                ),
+                              )),
+                          Padding(
+                              padding: const EdgeInsets.all(5.0),
+                              child: TextFormField(
+                                controller: _gradesCountController,
+                                keyboardType: TextInputType.number,
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.digitsOnly
+                                ],
+                                validator: (value) {
+                                  if (value == null || value.isEmpty) {
+                                    return snapshot.data!
+                                        .findAllElements(
+                                            "button_enter_grades_amount_text")
+                                        .first
+                                        .text;
+                                  }
+                                  if (int.parse(value) < 5 ||
+                                      int.parse(value) > 15) {
+                                    return snapshot.data!
+                                        .findAllElements(
+                                            "button_enter_grades_amount_error")
+                                        .first
+                                        .text;
+                                  }
+                                  return null;
+                                },
+                                decoration: InputDecoration(
+                                  labelText: snapshot.data!
+                                      .findAllElements(
+                                          "button_enter_grades_amount_label")
+                                      .first
+                                      .text,
+                                  hintText: snapshot.data!
+                                      .findAllElements(
+                                          "button_enter_grades_amount_hint")
+                                      .first
+                                      .text,
+                                  border: OutlineInputBorder(),
+                                ),
+                              )),
+                          Visibility(
+                              visible: _showSubmitButton,
+                              child: ElevatedButton(
+                                child: const Text('OCENY'),
+                                onPressed: () {
+                                  if (_formKey.currentState!.validate()) {
+                                    print(_nameController.text);
+                                  }
+                                },
+                              ))
+                        ],
+                      ))));
+        } else if (snapshot.hasError) {
+          // An error occurred, so show an error message
+          return Text('Error: ${snapshot.error}');
+        }
+        // Otherwise, show a loading indicator
+        return CircularProgressIndicator();
+      },
+    );
   }
 }
